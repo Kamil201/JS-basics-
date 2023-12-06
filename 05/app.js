0d9f82f1ad0dd0b3101965b500372d11c97a0ccc
Kamil Lewandowski <kamlew555@gmail.com>
2023-12-06 18:38:51 +0100

    finish exercise

--- 05/app.js
+++ 05/app.js
@@ -1,9 +1,19 @@
-function Student (initFirstName, initLastName){
-    this.initFirstName = initFirstName;
-    this.initLastName = initLastName;
+function Student (FirstName, LastName){
+    this.FirstName = FirstName;
+    this.LastName = LastName;
+    this.grades = {}
 }
 
 const firstStudent = new Student ('Jan', 'Nowak')
 const secondStudent = new Student ('Anna', 'Lewandowska')
 
+Student.prototype.addGrade = function (subject, value ){
+    if(typeof this.grades[subject] === "undefined"){
+        this.grades[subject] = []
+    }
+    this.grades[subject].push(value)
+}
+
+firstStudent.addGrade('math', 5)
+secondStudent.addGrade('biology', 4)
 console.log(firstStudent, secondStudent)
